7f619cdedf1df5a69fef2b01340c5b28029a3dd4
ThatMeme <95719622+github-for-meme@users.noreply.github.com>
2021-12-07 15:41:58 -0600

    Create one lineer function
    This function does the same thing as the class but like way better

---
+++ OneLiner.py
@@ -0,0 +1,1 @@
+print("www.multisoft.se/"+''.join([str(max([int(i) for i in "1112031584"][i], [int(i) for i in "1112031584"][i-1])) for i in range(1, len([int(i) for i in "1112031584"])) if [int(i) for i in "1112031584"][i] % 2== [int(i) for i in "1112031584"][i-1] % 2]))
